b918f52a6bb5ed25eacf0d90d22296f79dfa31f6
Mamaliga Dan <danmamaliga2@gmail.com>
2023-11-17 10:16:01 +0200

    added initial spring application

---
+++ src/test/java/gameloft/interview/profilematcher/ProfileMatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package gameloft.interview.profilematcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileMatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
