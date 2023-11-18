637947be404dd7e9aef49d537f87cdf7fdca1862
Mamaliga Dan <danmamaliga2@gmail.com>
2023-11-18 19:40:54 +0200

    removed jpa references

--- src/main/java/gameloft/interview/profilematcher/player/PlayerJpaRepository.java
+++
@@ -1,5 +0,0 @@
-package gameloft.interview.profilematcher.player;
-
-public interface PlayerJpaRepository {
-
-}
--- src/main/java/gameloft/interview/profilematcher/player/PlayerServiceImpl.java
+++ src/main/java/gameloft/interview/profilematcher/player/PlayerServiceImpl.java
@@ -1,13 +1,9 @@
 package gameloft.interview.profilematcher.player;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PlayerServiceImpl implements PlayerService {
-
-  @Autowired
-  public PlayerJpaRepository playerRepository;
   
   @Override
   public Player findPlayerById(String playerId) {
