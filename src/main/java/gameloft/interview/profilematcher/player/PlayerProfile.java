a89e88615248ee646e2804bac38b6a3bd4dce1d3
Mamaliga Dan <danmamaliga2@gmail.com>
2023-11-20 23:30:09 +0200

    cleanup PlayerProfile

--- src/main/java/gameloft/interview/profilematcher/player/PlayerProfile.java
+++ src/main/java/gameloft/interview/profilematcher/player/PlayerProfile.java
@@ -15,20 +15,15 @@ public record PlayerProfile(
     @Id
     //    @Field(value = "player_id")
     String playerId, String credential,
-    //    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZ")
     OffsetDateTime created,
-    //    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZ")
     OffsetDateTime modified,
-    //    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZ")
     @Field(value = "last_session") OffsetDateTime lastSession,
     @Field(value = "total_spent") BigDecimal totalSpent,
     @Field(value = "total_refund") BigDecimal totalRefund,
     @Field(value = "total_transactions") int totalTransactions,
-    //    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZ")
     @Field(value = "last_purchase") OffsetDateTime lastPurchase,
     @Field(value = "active_campaigns") List<String> activeCampaigns, List<Device> devices, int level, int xp,
     @Field(value = "total_playtime") int totalPlaytime, String country, String language,
-    //    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZ")
     OffsetDateTime birthdate,
     String gender, Inventory inventory,
     Clan clan,
